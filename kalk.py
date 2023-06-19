29465c46e80b96110767aedd51448c9f42cd898a
Tor Odin <tor-odin.ekle@capgemini.com>
2023-06-19 14:13:23 +0200

    start kalk

---
+++ kalk.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.header("Adrianas Kalkulator")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Sett inn åpne m2")
+    a = st.number_input("åpne m2", help="Sett inn åpne m2", value=0.0, step=0.1, key="open")
+
+with col2:
+    st.subheader("Sett inn lukkede m2")
+    b = st.number_input("lukkede m2", help="Sett inn lukkede m2", value=0.0, step=0.1, key="closed")
+
+st.subheader(f"Totalt åpne m2: {round((a/(a+b))*100,2)} %")
+st.subheader(f"Antall m2 for å få 30% åpenhet = {round((a+b)*0.3,2)} m2")
